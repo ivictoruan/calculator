54ce56b8981a0d032f42282f7a634a4c3cb9a0b2
Victor Pereira <victorruan135@gmail.com>
2021-09-09 15:34:20 -0300

    Correcting some errors. Buttons functionalities works! 👀👀✔✔✔

--- app/src/main/java/br/com/ecp/victor/calculator/MainActivity.java
+++ app/src/main/java/br/com/ecp/victor/calculator/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView txtExpression, txtResult;
     private ImageView backspace;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +25,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         StartComponents();
         getSupportActionBar().hide();
 
+        SetComponents();
+
+    }
+
+    private void StartComponents () {
+            numberZero = findViewById(R.id.number_zero);
+            numberOne = findViewById(R.id.number_one);
+            numberTwo = findViewById(R.id.number_two);
+            numberThree = findViewById(R.id.number_three);
+            numberFour = findViewById(R.id.number_four);
+            numberFive = findViewById(R.id.number_five);
+            numberSix = findViewById(R.id.number_six);
+            numberSeven = findViewById(R.id.number_seven);
+            numberEight = findViewById(R.id.number_eight);
+            numberNine =findViewById(R.id.number_nine);
+            dot = findViewById(R.id.bt_dot);
+            sum = findViewById(R.id.bt_sum);
+            multiplication = findViewById(R.id.bt_multiplication);
+            subtraction = findViewById(R.id.bt_subtraction);
+            division = findViewById(R.id.bt_division);
+            txtResult = findViewById(R.id.txt_result);
+            txtExpression = findViewById(R.id.txt_expression);
+            backspace = findViewById(R.id.backspace);
+            equal = findViewById(R.id.equal);
+            bt_clean = findViewById(R.id.bt_clean);
+    }
+
+    public void SetComponents(){
         numberZero.setOnClickListener(this);
         numberOne.setOnClickListener(this);
         numberTwo.setOnClickListener(this);
         numberThree.setOnClickListener(this);
         numberFour.setOnClickListener(this);
-        numberFive.setOnClickListener(this);
+        numberFive.setOnClickListener (this);
         numberSix.setOnClickListener(this);
         numberSeven.setOnClickListener(this);
         numberEight.setOnClickListener(this);
@@ -42,104 +69,107 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         subtraction.setOnClickListener(this);
         division.setOnClickListener(this);
 
-        bt_clean.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                txtExpression.setText("");
-                txtResult.setText("");
-            }
+        bt_clean.setOnClickListener(view -> {
+            txtExpression.setText("");
+            txtResult.setText("");
         });
 
-        backspace.setOnClickListener(new View.OnClickListener(){
+        backspace.setOnClickListener(view -> {
+            TextView expression = findViewById(R.id.txt_expression);
+            String string = expression.getText().toString();
 
-            @Override
-            public void onClick(View view) {
-                TextView expression = findViewById(R.id.txt_expression);
-                String string = expression.getText().toString();
-
-                if(!string.isEmpty()){
-
-                    byte var0 = 0;
-                    int var1 = string.length() - 1;
-                    String txtExpression = string.substring(var0, var1);
-                    expression.setText(txtExpression);
-                }
-                txtResult.setText("");
+            if(!string.isEmpty()){
+                byte var0 = 0;
+                int var1 = string.length() - 1;
+                String txtExpression = string.substring(var0, var1);
+                expression.setText(txtExpression);
             }
+            txtResult.setText("");
         });
     }
 
-    private void StartComponents () {
-
-    }
-    public void addExpression(String string, boolean clean_data) {//the second parametrer is a state
+    public void addExpression(String string, boolean clean_data) {
         if(txtResult.getText().equals("")) {
             txtExpression.setText("");
         }
         if(clean_data) {
             txtResult.setText("");
-            txtExpression.append(string);
 
         }else {
             txtExpression.append(txtResult.getText());
-            txtExpression.append(string);
-            txtResult.setText("");
         }
+        txtExpression.append(string);
     }
 
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.number_zero:
                 addExpression("0", true);
                 break;
+
             case R.id.number_one:
                 addExpression("1", true);
                 break;
+
             case R.id.number_two:
                 addExpression("2", true);
                 break;
+
             case R.id.number_three:
                 addExpression("3", true);
                 break;
+
             case R.id.number_four:
                 addExpression("4", true);
                 break;
+
             case R.id.number_five:
                 addExpression("5", true);
                 break;
+
             case R.id.number_six:
                 addExpression("6", true);
                 break;
+
             case R.id.number_seven:
                 addExpression("7", true);
                 break;
+
             case R.id.number_eight:
                 addExpression("8", true);
                 break;
+
             case R.id.number_nine:
                 addExpression("9", true);
                 break;
+
             case R.id.bt_clean:
                 addExpression("", true);
                 break;
+
             case R.id.bt_division:
                 addExpression("/", false);
                 break;
+
             case R.id.bt_multiplication:
                 addExpression("*", false);
                 break;
+
             case R.id.bt_sum:
                 addExpression("+", false);
                 break;
+
             case R.id.bt_subtraction:
                 addExpression("-", false);
                 break;
+
             case R.id.equal:
                 addExpression("=", false);
                 break;
-            case R.id.dot:
+
+            case R.id.bt_dot:
                 addExpression(".", true);
                 break;
         }
